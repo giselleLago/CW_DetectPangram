34e9fde022e9bd412ec72e1b63b71aee9ece38db
giselleLago <gisellelago25994@gmail.com>
2020-07-31 11:12:26 +0200

    Add project files.

---
+++ CW_DetectPangram/Kata.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CW_DetectPangram
+{
+    public class Kata
+    {
+        public bool IsPangram(string str)
+        {
+            return str.Where(ch => Char.IsLetter(ch)).Select(ch => Char.ToLower(ch)).Distinct().Count() == 26;
+        }
+    }
+}
---
+++ CW_DetectPangram/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CW_DetectPangram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var a = new Kata();
+            Console.WriteLine(a.IsPangram("Cwm fjord bank glyphs vext quiz"));
+            Console.ReadKey();
+        }
+    }
+}
